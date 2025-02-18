fc288ad4b4d402bb0d6f95954f80662e62e8a194
arthast <amtakhunov@edu.hse.ru>
2025-02-18 10:56:16 +0300

    add_tests_pi

---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arsen/cppprojects/LongNumber/googletest/googletest
+# Build directory: /home/arsen/cppprojects/LongNumber/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
