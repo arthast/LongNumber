5bc092cafc97ee92c30194bdc6fa804827e94bed
arthast <amtakhunov@edu.hse.ru>
2025-02-25 11:39:07 +0300

    fix3

---
+++ build/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arsen/cppprojects/LongNumber/my_tests/binary_dev_test.cpp" "CMakeFiles/tests.dir/my_tests/binary_dev_test.cpp.o" "gcc" "CMakeFiles/tests.dir/my_tests/binary_dev_test.cpp.o.d"
+  "/home/arsen/cppprojects/LongNumber/my_tests/binary_mult_test.cpp" "CMakeFiles/tests.dir/my_tests/binary_mult_test.cpp.o" "gcc" "CMakeFiles/tests.dir/my_tests/binary_mult_test.cpp.o.d"
+  "/home/arsen/cppprojects/LongNumber/my_tests/binary_plus_test.cpp" "CMakeFiles/tests.dir/my_tests/binary_plus_test.cpp.o" "gcc" "CMakeFiles/tests.dir/my_tests/binary_plus_test.cpp.o.d"
+  "/home/arsen/cppprojects/LongNumber/my_tests/binary_sub_test.cpp" "CMakeFiles/tests.dir/my_tests/binary_sub_test.cpp.o" "gcc" "CMakeFiles/tests.dir/my_tests/binary_sub_test.cpp.o.d"
+  "/home/arsen/cppprojects/LongNumber/my_tests/bool_test.cpp" "CMakeFiles/tests.dir/my_tests/bool_test.cpp.o" "gcc" "CMakeFiles/tests.dir/my_tests/bool_test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/googletest-main/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/arsen/cppprojects/LongNumber/googletest-main
+# Build directory: /home/arsen/cppprojects/LongNumber/build/googletest-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
