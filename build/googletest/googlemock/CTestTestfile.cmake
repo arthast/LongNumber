c85410c678fe4e8c43a3d89f21b8046824bec4e2
arthast <amtakhunov@edu.hse.ru>
2025-02-25 11:29:14 +0300

    fix

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arsen/cppprojects/LongNumber/googletest/googlemock
-# Build directory: /home/arsen/cppprojects/LongNumber/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arsen/cppprojects/LongNumber/googletest/googletest
-# Build directory: /home/arsen/cppprojects/LongNumber/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
