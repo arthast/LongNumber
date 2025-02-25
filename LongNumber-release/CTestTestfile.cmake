a96b5b992cd02ba1cdfc6252491471067ccb545c
arthast <amtakhunov@edu.hse.ru>
2025-02-25 12:02:53 +0300

    fix5

---
+++ LongNumber-release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/arsen/cppprojects/LongNumber
+# Build directory: /home/arsen/cppprojects/LongNumber/LongNumber-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("googletest-main")
---
+++ LongNumber-release/googletest-main/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arsen/cppprojects/LongNumber/googletest-main/googletest
+# Build directory: /home/arsen/cppprojects/LongNumber/LongNumber-release/googletest-main/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
